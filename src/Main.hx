7ba95784d798d7699fa689308b075a01c1c3910d
brutalexcess <selman.luke1@gmail.com>
2015-08-20 18:15:16 +0100

    Menus and event handlers

--- src/Main.hx
+++ src/Main.hx
@@ -4,11 +4,18 @@ import cpp.Lib;
 
 import wx.Frame;
 import wx.App;
+import wx.Menu;
+import wx.MenuBar;
+import wx.MessageDialog;
+import wx.StaticText;
 
 class Main
 {
 
 	private var _mainFrame:Frame;
+	private var _mainMenuBar:MenuBar;
+	private var _fileMenu:Menu;
+	private var _lblGeneric:StaticText;
 
 	public function new()
 	{
@@ -16,6 +23,26 @@ class Main
 
 		App.setTopWindow(_mainFrame);
 		_mainFrame.shown = true;
+
+		_mainMenuBar = new MenuBar();
+
+		_fileMenu = new Menu("");
+		_fileMenu.append(1, "Show Message");
+
+		_lblGeneric = StaticText.create(_mainFrame, 2, "Hello, there.");
+
+		_mainMenuBar.append(_fileMenu, "File");
+
+		_mainFrame.handle(1, function(e)
+		{
+			var response = new MessageDialog(_mainFrame, "You clicked on Show Message.", "Message", MessageDialog.YES_NO);
+			if (response.showModal() == 5103)
+			{
+				_lblGeneric.set_label("You clicked on yes.");
+			}
+		});
+
+		_mainFrame.wxSetMenuBar(_mainMenuBar);
 	}
 	
 	static function main() 
